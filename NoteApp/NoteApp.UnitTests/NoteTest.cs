54433a8937c2e2fdee17eea50ae63f4f792e7b5a
Chshedrin Andrey <shedrin2001@mail.ru>
2020-07-10 17:25:20 +0700

    feature(NoteApp.UnitTests): созданы классы для Unit-тестирования:
    -NoteTest
    -ProjectManagerTest
    -ProjectTest

---
+++ NoteApp/NoteApp.UnitTests/NoteTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NoteApp;
+
+namespace NotesApp.UnitTests
+{
+    [TestFixture]
+    public class NoteTest
+    {
+        [Test(Description = "Позитивный тест геттера Title")]
+        public void TestTitleGet_CorrectValue()
+        {
+            var expected = "NoteTitle";
+            var note = new Note();
+            note.Title = expected;
+            var actual = note.Title;
+            Assert.AreEqual(expected, actual, "Геттер Title возвращает неправильную фамилию");
+
+        }
+
+    }
+}
